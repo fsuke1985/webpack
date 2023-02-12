520a96df0eb057edc424f99ca917c2b0f40cc903
fsuke1985 <fsuke1985@gmail.com>
2023-02-12 15:44:25 +0900

    add devcontainer

--- webpack.config.js
+++ webpack.config.js
@@ -1,88 +1,84 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const isProduction = process.env.NODE_ENV == 'production';
+const isProduction = process.env.NODE_ENV == "production";
 
-
-const stylesHandler = 'style-loader';
-
-
+const stylesHandler = "style-loader";
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        },
+  },
+  devServer: {
+    open: true,
+    static: {
+      directory: path.join(__dirname, "public"),
     },
-    devServer: {
-        open: true,
-        static: {
-            directory: path.join(__dirname, 'public')
-        },
-        compress: true,
-        port: 9999,
-        watchFiles: ['dist/**/*'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
+    compress: true,
+    port: 9999,
+    watchFiles: ["dist/**/*"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
 
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+
+              {
+                targets: "defaults",
+                useBuiltIns: "usage",
+                corejs: 3,
+              },
+            ],
+          ],
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        [
-                            "@babel/preset-env",
-                            {
-                                targets: 'defaults',
-                                useBuiltIns: 'usage',
-                                corejs: 3
-                            }
-                        ]
-                    ]
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
+  },
 };
 
 module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-
-
-    } else {
-        config.mode = 'development';
-        config.devtool = 'inline-source-map';
-        config.watch = true;
-        config.watchOptions = {
-            ignored: '**/node_modules',
-        };
-    }
-    return config;
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+    config.devtool = "inline-source-map";
+    config.watch = true;
+    config.watchOptions = {
+      ignored: "**/node_modules",
+    };
+  }
+  return config;
 };
